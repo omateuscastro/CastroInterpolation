9a496c6710e04b18c8f78e22777ffdbba7ae70d9
Mateus Castro <mcastro.mateus@gmail.com>
2021-03-10 22:43:27 -0300

    Adicionado parametro para escolha da precisão do retorno do calculo

--- CastroInterpolation.Tests/CastroInterpolationTests.cs
+++ CastroInterpolation.Tests/CastroInterpolationTests.cs
@@ -8,14 +8,15 @@ namespace CastroInterpolation.Tests
     {
         [TestCategory("Interpolation")]
         [TestMethod("Should Validate Linear Interpolation")]
-        [DataRow(30, 3, 40, 5, 37, 4.4)]
-        [DataRow(48, 827.55, 60, 1034.1677, 50, 861.9863)]
-        [DataRow(10, 190, 48, 827.55, 30, 525.5526)]
-        [DataRow(10, 56.22, 48, 112.44, 45, 108.0016)]
-        public void ShouldValidateInterpolation(double x1, double y1, double x2, double y2, double x3, double expectedResult)
+        [DataRow(30, 3, 40, 5, 37, 4.4, 2)]
+        [DataRow(48, 827.55, 60, 1034.1677, 50, 861.9863, 4)]
+        [DataRow(48, 827.55, 60, 1034.1677, 50, 861.99, 2)]
+        [DataRow(10, 190, 48, 827.55, 30, 525.5526, 4)]
+        [DataRow(10, 56.22, 48, 112.44, 45, 108.0016, 4)]
+        public void ShouldValidateInterpolation(double x1, double y1, double x2, double y2, double x3, double expectedResult, int precision)
         {   
 
-            Assert.AreEqual(Convert.ToDecimal(expectedResult), CastroInterpolation.CalculateInterpolation(Convert.ToDecimal(x1), Convert.ToDecimal(y1), Convert.ToDecimal(x2), Convert.ToDecimal(y2), Convert.ToDecimal(x3)));
+            Assert.AreEqual(Convert.ToDecimal(expectedResult), CastroInterpolation.CalculateInterpolation(Convert.ToDecimal(x1), Convert.ToDecimal(y1), Convert.ToDecimal(x2), Convert.ToDecimal(y2), Convert.ToDecimal(x3), precision));
         }
     }
 }
