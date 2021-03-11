6fe5c734271421070299dae8ccf4dbcce19960d3
Mateus Castro <mcastro.mateus@gmail.com>
2021-03-10 23:30:30 -0300

    Adicionada opção de parametros Y com inteiro.

--- CastroInterpolation/CastroInterpolation.cs
+++ CastroInterpolation/CastroInterpolation.cs
@@ -2,7 +2,7 @@
 
 namespace CastroInterpolation
 {
-    public static class CastroInterpolation
+    public static class LinearInterpolation
     {
         public static decimal? CalculateInterpolation(decimal x1, decimal y1, decimal x2, decimal y2, decimal x3, int precision = 4){
             try
@@ -15,6 +15,25 @@ namespace CastroInterpolation
                     param = (x3 - x1) / (x2 - x1);
 
                     return Math.Round(y1 + param * (y2 - y1), precision);
+            }
+            catch (System.Exception)
+            {
+                return null;
+            }
+        }
+
+        public static int? CalculateInterpolation(decimal x1, int y1, decimal x2, int y2, decimal x3, int precision = 4)
+        {
+            try
+            {
+                decimal param;
+
+                if (x2 == x1)
+                    param = 0;
+                else
+                    param = (x3 - x1) / (x2 - x1);
+
+                return Convert.ToInt32(Math.Round(y1 + param * (y2 - y1), precision));
             }
             catch (System.Exception)
             {
